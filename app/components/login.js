da23d978ce524eb67afc577dc563621b880b930b
alikamatu <alikamaatu14@gmail.com>
2024-11-03 13:51:06 +0000

    Update login.js

--- app/components/login.js
+++ app/components/login.js
@@ -3,7 +3,7 @@ import { FcGoogle } from "react-icons/fc";
 function Login() {
   const handleGoogleLogin = () => {
     window.location.href = "http://localhost:5000/auth/google";
-
+  }
   return (
     <div>
         <button
