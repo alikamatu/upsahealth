8a510e3f966d02896066f62f1aaebb992c0734cc
alikamatu <122170675+alikamatu@users.noreply.github.com>
2024-11-03 18:11:41 +0000

    Update login.js

--- app/components/login.js
+++ app/components/login.js
@@ -2,7 +2,7 @@ import { FcGoogle } from "react-icons/fc";
 
 function Login() {
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:5000/auth/google";
+    window.location.href = "https://upsahealth.vercel.app/auth/google";
   }
   return (
     <div>
