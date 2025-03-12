6e502a6f152de59aa4982ad17aa38948986fa31f
alikamatu_devrel <122170675+alikamatu@users.noreply.github.com>
2025-03-12 19:33:46 +0000

    api redirecting

--- app/login/page.js
+++ app/login/page.js
@@ -31,7 +31,8 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      // const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://healthbackend.vercel.app/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
